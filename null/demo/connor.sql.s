c4be9f213adddfd534b080ee9cbc0b7697d19e76
Patrick Jolliffe <jolliffe@gmail.com>
2025-08-06 14:56:58 +0100

    this is finished presentations only

--- null/demo/connor.sql.s
+++
@@ -1,9 +0,0 @@
-create table t as select * From dba_objects;
-
-variable search_criteria number
-exec :search_criteria := 123
-
-create index ix1 on t ( object_id ) ;
-
-select * from t where object_id = nvl(:search_criteria, object_id);
-
